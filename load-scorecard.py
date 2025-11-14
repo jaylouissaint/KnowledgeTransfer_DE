6f8308d31d958af9ca36a487d6de1320c957df05
Jasmine Kwok <shantong.k@gmail.com>
2025-11-14 14:15:27 -0500

    Added driver code to load scorecard file

--- load-scorecard.py
+++ load-scorecard.py
@@ -1,55 +1,60 @@
 # Your driver code (including input / output handling) should go here
 import sys
-import psycopg
-import credentials
-from cleaning-collegescorecard
-from collegescorecard_utils
+import sql_queries as query
+import cleaning_collegescorecard as clean_cs
+import collegescorecard_utils as utils
+import time
+import re
+
 
 def main():
-    # read inserted file 
-    # needs to capture if wrong file is given 
+    # Get csv filename
+    if len(sys.argv) < 2:
+        print("Error: Missing CSV file argument.")
+        sys.exit(1)
+    filename = sys.argv[1]
+    # get the year from the filename
+    match = re.search(r"(\d{4})_(\d{2})", filename)
+    start, end = match.groups()
+    year = f"{start}-{start[:2]}{end}"
+
     try:
-        filename = sys.argv[1]
-    except (IndexError, ValueError):
-        print("Missing csv file")
+        start_time = time.time()
+        # Load csv data into a df
+        scorecard_data = utils.load_data(filename)
+
+        # clean data
+        institutions_clean = clean_cs.clean_institutions(scorecard_data)
+        academics_clean = clean_cs.clean_academics(scorecard_data, year)
+        demographics_clean = clean_cs.clean_demographics(scorecard_data, year)
+        financials_clean = clean_cs.clean_financials(scorecard_data, year)
+
+        print("Data cleaned successfully.")
+
+        # create the tables if they do not exist
+        utils.create_table(query.CREATE_INSTITUTIONS)
+        utils.create_table(query.CREATE_ACADEMICS)
+        utils.create_table(query.CREATE_FINANCIALS)
+        utils.create_table(query.CREATE_DEMOGRAPHICS)
+
+        print("All necessary tables created or already exists.")
+
+        # insert the data into the tables
+        utils.insert_data(query.INSERT_INSTITUTIONS, institutions_clean)
+        utils.insert_data(query.INSERT_FINANCIALS, financials_clean)
+        utils.insert_data(query.INSERT_DEMOGRAPHICS, demographics_clean)
+        utils.insert_data(query.INSERT_ACADEMICS, academics_clean)
+
+        print("Data loading complete.")
+
+        # Calculate time elapsed to load this file
+        elapsed_time = time.time() - start_time
+        print(f"{elapsed_time} taken to load data file.")
+
+    except Exception as e:
+        print("ETL Pipeline failed:", e)
         sys.exit(1)
 
-    try: 
-        # Establish a connection to the database
-        conn = psycopg.connect(host="debprodserver.postgres.database.azure.com",
-                            dbname=credentials.DB_USER,
-                            user=credentials.DB_USER,
-                            password=credentials.DB_PASSWORD)
-        cur = conn.cursor()
-
-        # this persona does not exist in personae table so it will
-        # lead to a foreign key constraint violation
-        query = "INSERT INTO events (moment, persona, element, score)" \
-                "VALUES ('2015-03-22 20:21:28.248253', 2000, 29407, 400);"
-
-        # clean data 
-        clean_data = clean_ipeds(data)
-
-        # insert data 
-        for index, row in df.iterrows():
-            cur.execute(query)
-
-        # commit all changes 
-        conn.commit()
-        print("Data loaded successfully.")
-        
-    except psycopg.IntegrityError as e:
-        print(f"Foreign key constraint violation detected: {e}")
-        conn.rollback()
-    except Exception as e:
-        # if something fails, undo all the changes 
-        conn.rollback()
-        # print the error we receive
-        print(f"Error: {e}")
-    finally: 
-        # close the connection
-        cur.close()
-        conn.close()
 
 if __name__ == "__main__":
     main()
